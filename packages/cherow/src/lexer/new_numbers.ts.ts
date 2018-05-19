13bf87af28f82edbca7f1fbda2d3bfc7d77df7e6
Kenny F <kfrachelle123@gmail.com>
2018-05-19 18:05:16 +0800

    refactor(cherow) added work in progress code - untested!!

--- packages/cherow/src/lexer/new_numbers.ts.ts
+++ packages/cherow/src/lexer/new_numbers.ts.ts
@@ -4,48 +4,127 @@ import { Chars } from '../chars';
 import { Context } from '../utilities';
 import { nextUnicodeChar, consumeOpt } from './common';
 import { Errors, recordErrors } from './errors';
+import { isIdentifierStart } from '../unicode';
 
-export function scanNumeric(parser: Parser, context: Context, first: number): Token {
+/**
+ * Scans numeric liteal
+ *
+ * @param parser Parser object
+ * @param context Context masks
+ */
+export function scanNumeric(parser: Parser, context: Context): Token {
     const { index } = parser;
-
-    scanDigits(parser);
-
-    if (consumeOpt(parser, Chars.Period)) {
-        scanDigits(parser);
+    const maximumDigits = 10;
+    let hasFloat = false;
+    let digit = maximumDigits - 1;
+    let seenSeparator = false;
+    let isBigInt = false;
+    let value: string | number = 0;
+    let code = parser.source.charCodeAt(parser.index);
+    while ((code >= Chars.Zero && code <= Chars.Nine || code === Chars.Underscore) && digit >= 0) {
+        if (code === Chars.Underscore) {
+            parser.index++;
+            parser.column++;
+            if (parser.source.charCodeAt(parser.index) === Chars.Underscore) {
+                return Token.Illegal;
+            }
+            seenSeparator = true;
+            continue;
+        }
+        seenSeparator = false;
+        value = 10 * value + (code - Chars.Zero);
+        parser.index++;
+        parser.column++;
+        code = parser.source.charCodeAt(parser.index);
+        --digit;
     }
 
-    let end = parser.index;
+    code = parser.source.charCodeAt(parser.index);
+
+    if (digit >= 0 && code !== Chars.Period && !isIdentifierStart(code) && code !== Chars.LowerN) {
+        parser.tokenValue = value;
+        return Token.NumericLiteral;
+    }
+
+    if (consumeOpt(parser, Chars.Period)) {
+        if (context & Context.OptionsNext && parser.source.charCodeAt(parser.index) === Chars.Underscore) {
+            recordErrors(parser, Errors.ZeroDigitNumericSeparator);
+        }
+        hasFloat = true;
+        value = `${value}.${scanDecimalDigitsOrSeparator(parser)}`;
+    }
+
+    const end = parser.index;
+
+    if (consumeOpt(parser, Chars.LowerN)) {
+        if (hasFloat) recordErrors(parser, Errors.Unexpected);
+        isBigInt = true;
+    }
+
     let next = parser.source.charCodeAt(parser.index);
 
     if (consumeOpt(parser, Chars.UpperE) || consumeOpt(parser, Chars.LowerE)) {
         next = parser.source.charCodeAt(parser.index);
         if (next === Chars.Plus || next === Chars.Hyphen) {
-            parser.index++;
-            parser.column++;
+            parser.index++;  parser.column++;
             next = parser.source.charCodeAt(parser.index);
         }
 
-        if (next >= Chars.Zero && next <= Chars.Nine) {
-            parser.index++; parser.column++;
-            scanDigits(parser);
-            end = parser.index;
-        } else {
-            recordErrors(parser, Errors.UnterminatedString);
-            return Token.Illegal;
+        if (!(next >= Chars.Zero && next <= Chars.Nine)) {
+            recordErrors(parser, Errors.Unexpected);
         }
+
+        const preNumericPart = parser.index;
+        const finalFragment = scanDecimalDigitsOrSeparator(parser);
+        value = parser.source.substring(end, preNumericPart) + finalFragment;
+    }
+
+    if (isIdentifierStart(parser.source.charCodeAt(parser.index))) {
+        recordErrors(parser, Errors.Unexpected);
     }
 
     parser.tokenValue = parser.source.slice(index, parser.index);
-    return Token.NumericLiteral;
+    return isBigInt ? Token.BigIntLiteral : Token.NumericLiteral;
 }
 
-function scanDigits(parser: Parser): void {
-    let code = parser.source.charCodeAt(parser.index);
-    while (code >= Chars.Zero && code <= Chars.Nine) {
+/**
+ * Scans decimal digit or numeric separator
+ *
+ * @param parser Parser object
+ * @param context Context masks
+ */
+function scanDecimalDigitsOrSeparator(parser: Parser): string {
+    let { index } = parser;
+    let allowSeparator = false;
+    let isPreviousTokenSeparator = false;
+    let result = '';
+    const code = parser.source.charCodeAt(parser.index);
+    while (code >= Chars.Zero && code <= Chars.Nine || code === Chars.Underscore) {
+        if (code === Chars.Underscore) {
+            if (allowSeparator) {
+                allowSeparator = false;
+                isPreviousTokenSeparator = true;
+                result += parser.source.substring(index, parser.index);
+            } else if (isPreviousTokenSeparator) {
+                recordErrors(parser, Errors.TrailingNumericSeparator);
+            } else {
+                recordErrors(parser, Errors.ContinuousNumericSeparator);
+            }
+            parser.index++;
+            parser.column++;
+            index = parser.index;
+            continue;
+        }
+        allowSeparator = true;
+        isPreviousTokenSeparator = false;
         parser.index++;
         parser.column++;
-        code = parser.source.charCodeAt(parser.index);
     }
+
+    if (parser.source.charCodeAt(parser.index - 1) === Chars.Underscore) {
+        recordErrors(parser, Errors.ContinuousNumericSeparator);
+    }
+    return result + parser.source.substring(index, parser.index);
 }
 
 function scanOctalDigits(parser: Parser, context: Context): void {}
