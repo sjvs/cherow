464fd214db8edc2a157a8e8b64777deb3b24eab1
Kenny F <kfrachelle123@gmail.com>
2018-05-21 15:16:04 +0800

    fixed string literal scanning

--- packages/cherow/src/lexer/new_string.ts
+++ packages/cherow/src/lexer/new_string.ts
@@ -3,74 +3,79 @@ import { Token } from '../token';
 import { Chars } from '../chars';
 import { Context, Flags } from '../utilities';
 import { peekUnicodeEscape, scanHexNumber } from './identifier';
-import { nextUnicodeChar, fromCodePoint } from './common';
+import { fromCodePoint, advance, toHex, nextUnicodeChar, readNext } from './common';
 import { Errors, recordErrors } from './errors';
 
 const enum Recovery {
     Empty = -1,
     StrictOctal = -2,
-    EightOrNine = -3
+    EightOrNine = -3,
+    InvalidHex = -4,
+    OutOfRange = -5,
 }
+
 /**
- * Scan string literal
+ * Throws a string error for either string or template literal
  *
- * @export
  * @param parser Parser object
  * @param context Context masks
- * @param first code point
-  */
+ */
+export function throwStringError(parser: Parser, context: Context, code: Recovery): Token {
+    let message: Errors = Errors.Unexpected;
+    if (Recovery.Empty) return;
+    if (Recovery.StrictOctal) message = Errors.StrictOctalEscape;
+    if (Recovery.EightOrNine) message = Errors.InvalidEightAndNine;
+    recordErrors(parser, Errors.UnterminatedString);
+    return Token.Illegal;
+}
 
+/**
+ * Scan a string literal
+ *
+ * @see [Link](https://tc39.github.io/ecma262/#sec-literals-string-literals)
+ *
+ * @param parser Parser object
+ * @param context Context masks
+ * @param quote codepoint
+ */
 export function scanString(parser: Parser, context: Context, quote: number): Token {
-    parser.index++; parser.column++;
-    let result = '';
-    let { index: start } = parser;
+    parser.index++; parser.column++; // consume quote
+    let { index, column } = parser;
+    let ret = '';
     let ch = parser.source.charCodeAt(parser.index);
+    loop:
+        while (parser.index < parser.source.length) {
+            ch = parser.source.charCodeAt(parser.index);
+            switch (ch) {
 
-    while (parser.index < parser.source.length) {
-        ch = parser.source.charCodeAt(parser.index);
-        switch (ch) {
-            case Chars.Backslash:
-                {
-                    result += parser.source.slice(start, parser.index);
-                    parser.index++; parser.column++;
-                    ch = parser.source.charCodeAt(parser.index);
-                    if (ch >= 128) {
-                        result += fromCodePoint(ch);
+                case Chars.LineSeparator:
+                case Chars.ParagraphSeparator:
+                case Chars.CarriageReturn:
+                case Chars.LineFeed:
+                    break loop;
+                case Chars.Backslash:
+                    ret += parser.source.slice(index, parser.index);
+                    ch = readNext(parser, ch);
+                    if (ch > Chars.MaxAsciiCharacter) {
+                        ret += fromCodePoint(ch);
                     } else {
                         const code = table[ch](parser, context, ch);
-                        if (code >= 0) {
-                            result += fromCodePoint(code);
-                            start = parser.index;
-                            ch = parser.source.charCodeAt(parser.index);
-                        } else {
-                            return handleRecoveryErrors(parser, code as Recovery);
-                        }
+                        if (code >= 0) ret += fromCodePoint(code);
+                        else throwStringError(parser, context, code as Recovery);
+                        index = parser.index + 1;
+                        column = parser.column + 1;
                     }
                     break;
-                }
-
-            case quote:
-                result += parser.source.slice(start, parser.index);
-                parser.index++;
-                parser.column++; // consume the quote
-                if (context & Context.OptionsRaw) parser.source.slice(start, parser.index);
-                parser.tokenValue = result;
-                return Token.StringLiteral;
-
-            case Chars.LineSeparator:
-                if (context & Context.OptionsNext) continue;
-                // falls through
-            case Chars.ParagraphSeparator:
-            case Chars.CarriageReturn:
-            case Chars.LineFeed:
-                parser.flags |= Flags.Unterminated;
-                recordErrors(parser, Errors.UnterminatedString);
-                return Token.Illegal;
-            default:
-                parser.index++;
-                parser.column++;
+                case quote:
+                    ret += parser.source.slice(index, parser.index);
+                    parser.index++; parser.column++; // consume the quote
+                    if (context & Context.OptionsRaw) parser.source.slice(index, parser.index);
+                    parser.tokenValue = ret;
+                    return Token.StringLiteral;
+                default:
+                    parser.index++; parser.column++;
+            }
         }
-    }
 
     // Unterminated string literal
     parser.flags |= Flags.Unterminated;
@@ -78,31 +83,7 @@ export function scanString(parser: Parser, context: Context, quote: number): Tok
     return Token.Illegal;
 }
 
-/**
- * Handles recovery errors.
- *
- * @param parser Object
- * @param code Errors code
- */
-function handleRecoveryErrors(parser: Parser, code: Recovery): Token {
-    // We have to keep going, so advance
-    parser.index++; parser.column++;
-    let message;
-    // TODO: Record errors for this cases
-    switch (code) {
-        case Recovery.StrictOctal:
-            message = Errors.StrictOctalEscape;
-            break;
-        case Recovery.EightOrNine:
-            message = Errors.InvalidEightAndNine;
-            break;
-        default:
-    }
-    recordErrors(parser, Errors.UnterminatedString);
-    return Token.Illegal;
-}
-
-export const table = new Array<(parser: Parser, context: Context, first: number) => number> (128).fill(nextUnicodeChar);
+const table = new Array<(parser: Parser, context: Context, first: number) => number>(128).fill(nextUnicodeChar);
 
 table[Chars.LowerB] = () => Chars.Backspace;
 table[Chars.LowerF] = () => Chars.FormFeed;
@@ -115,12 +96,13 @@ table[Chars.CarriageReturn] = (parser: Parser) => {
     parser.column = -1;
     parser.line++;
 
-    const { index } = parser;
+    const {index} = parser;
 
     if (index < parser.source.length) {
         const ch = parser.source.charCodeAt(index);
 
         if (ch === Chars.LineFeed) {
+            parser.lastChar = ch;
             parser.index = index + 1;
         }
     }
@@ -129,86 +111,123 @@ table[Chars.CarriageReturn] = (parser: Parser) => {
 };
 
 table[Chars.LineFeed] =
-    table[Chars.LineSeparator] =
-    table[Chars.ParagraphSeparator] = (parser: Parser) => {
-        parser.column = -1;
-        parser.line++;
-        return Recovery.Empty;
-    };
+table[Chars.LineSeparator] =
+table[Chars.ParagraphSeparator] = (parser: Parser) => {
+    parser.column = -1;
+    parser.line++;
+    return Recovery.Empty;
+};
 
+// Null character, octals
 table[Chars.Zero] =
-    table[Chars.One] =
-    table[Chars.Two] =
-    table[Chars.Three] = (parser, context, first) => {
-        let code = first - Chars.Zero;
-        let index = parser.index + 1;
-        let column = parser.column + 1;
+table[Chars.One] =
+table[Chars.Two] =
+table[Chars.Three] = (parser, context, first) =>   {
+    // 1 to 3 octal digits
+    let code = first - Chars.Zero;
+    let index = parser.index + 1;
+    let column = parser.column + 1;
 
-        if (index < parser.source.length) {
-            const next = parser.source.charCodeAt(index);
+    let next = parser.source.charCodeAt(index);
 
-            if (next < Chars.Zero || next > Chars.Seven) {
-                // Strict mode code allows only \0, then a non-digit.
-              if (code !== 0 || next === Chars.Eight || next === Chars.Nine) {
-                  if (context & Context.Strict) return Recovery.StrictOctal;
-              }
-            } else if (context & Context.Strict) {
-                // This happens in cases like `\00` in strict mode.
-                return Recovery.StrictOctal;
-            } else {
-                code = (code << 3) | (next - Chars.Zero);
-                index++;
-                column++;
+    if (next < Chars.Zero || next > Chars.Seven) {
 
-                if (index < parser.source.length) {
-                    const next = parser.source.charCodeAt(index);
-
-                    if (next >= Chars.Zero && next <= Chars.Seven) {
-                        code = (code << 3) | (next - Chars.Zero);
-                        index++;
-                        column++;
-                    }
-                }
-
-                parser.index = index - 1;
-                parser.column = column - 1;
-            }
+        // Strict mode code allows only \0, then a non-digit.
+        if (code !== 0 || next === Chars.Eight || next === Chars.Nine) {
+            if (context & Context.Strict) return Recovery.StrictOctal;
+            parser.flags |= Flags.HasOctal;
+        }
+    } else if (context & Context.Strict) {
+        return Recovery.StrictOctal;
+    } else {
+        parser.flags |= Flags.HasOctal;
+        parser.lastChar = next;
+        code = code * 8 + (next - Chars.Zero);
+        index++;
+        column++;
+        next = parser.source.charCodeAt(index);
+        if (next >= Chars.Zero && next <= Chars.Seven) {
+            parser.lastChar = next;
+            code = code * 8 + (next - Chars.Zero);
+            index++;
+            column++;
         }
 
-        return code;
-    };
+        parser.index = index - 1;
+        parser.column = column - 1;
+    }
 
+    return code;
+}
 table[Chars.Four] =
-    table[Chars.Five] =
-    table[Chars.Six] =
-    table[Chars.Seven] = (parser, context, first) => {
-        if (context & Context.Strict) return Recovery.StrictOctal;
-        let code = first - Chars.Zero;
-        const index = parser.index + 1;
-        const column = parser.column + 1;
+table[Chars.Five] =
+table[Chars.Six] =
+table[Chars.Seven] = (parser, context, first) => {
+    if (context & Context.Strict) return Recovery.StrictOctal;
+    let code = first - Chars.Zero;
+    const index = parser.index + 1;
+    const column = parser.column + 1;
 
-        if (index < parser.source.length) {
-            const next = parser.source.charCodeAt(index);
+    if (index < parser.source.length) {
+        const next = parser.source.charCodeAt(index);
 
-            if (next >= Chars.Zero && next <= Chars.Seven) {
-                code = (code << 3) | (next - Chars.Zero);
-                parser.index = index;
-                parser.column = column;
-            }
+        if (next >= Chars.Zero && next <= Chars.Seven) {
+            code = (code << 3) | (next - Chars.Zero);
+            parser.lastChar = next;
+            parser.index = index;
+            parser.column = column;
         }
+    }
 
-        return code;
-    };
+    return code;
+};
 
 // `8`, `9` (invalid escapes)
 table[Chars.Eight] = table[Chars.Nine] = () => Recovery.EightOrNine;
 
-table[Chars.LowerU] = (parser, context, prev) => {
-    parser.index++; parser.column++;
-    return peekUnicodeEscape(parser, context);
+// ASCII escapes
+table[Chars.LowerX] = (parser, _, first) => {
+    const ch1 = parser.lastChar = readNext(parser, first);
+    const hi = toHex(ch1);
+    if (hi < 0) return Recovery.InvalidHex;
+    const ch2 = parser.lastChar = readNext(parser, ch1);
+    const lo = toHex(ch2);
+    if (lo < 0) return Recovery.InvalidHex;
+
+    return hi << 4 | lo;
 };
 
-table[Chars.LowerX] = (parser, context, first) => {
-    parser.index++; parser.column++;
-    return scanHexNumber(parser, 2);
+// UCS-2/Unicode escapes
+table[Chars.LowerU] = (parser, _, prev) =>  {
+    let ch = parser.lastChar = readNext(parser, prev);
+    if (ch === Chars.LeftBrace) {
+        ch = parser.lastChar = readNext(parser, ch);
+        let code = toHex(ch);
+        if (code < 0) return Recovery.InvalidHex;
+
+        ch = parser.lastChar = readNext(parser, ch);
+        while (ch !== Chars.RightBrace) {
+            const digit = toHex(ch);
+            if (digit < 0) return Recovery.InvalidHex;
+            code = code * 16 + digit;
+            // Code point out of bounds
+            if (code > Chars.NonBMPMax) return Recovery.OutOfRange;
+            ch = parser.lastChar = readNext(parser, ch);
+        }
+
+        return code;
+    } else {
+        // \uNNNN
+        let codePoint = toHex(ch);
+        if (codePoint < 0) return Recovery.InvalidHex;
+
+        for (let i = 0; i < 3; i++) {
+            ch = parser.lastChar = readNext(parser, ch);
+            const digit = toHex(ch);
+            if (digit < 0) return Recovery.InvalidHex;
+            codePoint = codePoint * 16 + digit;
+        }
+
+        return codePoint;
+    }
 };
---
+++ packages/cherow/test/lexer/string.ts
@@ -0,0 +1,242 @@
+import * as t from 'assert';
+import { scan } from '../../src/lexer/scan';
+import { fromCodePoint } from '../../src/lexer/common';
+import { createParserObject } from '../../src/parser/parser';
+import { Context, Recovery } from '../../src/utilities';
+import { parseSource, parse } from '../../src/cherow';
+import { Token, tokenDesc } from '../../src/token';
+import * as assert from 'clean-assert';
+
+describe('Lexer - String literals', () => {
+
+    describe('Editor mode', () => {
+
+        describe(`Early errors`, () => {
+            it(`should move on if unterminated string literal`, () => {
+                const errArray: any = [];
+                const res = parse('"unterminated string', {
+                    next: true
+                }, (err: string, line: number, column: number) => {
+                    errArray.push(`Line ${line}, column ${column}: ${err}`);
+                });
+                assert.match(errArray[0], 'Line 1, column 20: Unterminated string literal');
+            });
+
+            it(`should move on if unterminated string on multiple lines`, () => {
+                const errArray: any = [];
+                const res = parse(`"
+                H
+                e
+                l
+                l
+                o
+                "`, {
+                    next: true
+                }, (err: string, line: number, column: number) => {
+                    errArray.push(`Line ${line}, column ${column}: ${err}`);
+                });
+                // Note: Unterminated strings are counted from quote to quote - or the last letter
+                // where the missing quote should have been.
+                // Invalid, but not unterminated string literal - across multiple lines are
+                // counted from quote to quote
+                assert.match(errArray[0], 'Line 1, column 1: Unterminated string literal');
+                assert.match(errArray[1], 'Line 7, column 17: Unterminated string literal');
+            });
+
+        });
+    });
+
+    describe('Pass', () => {
+
+        function pass(name: string, opts: any) {
+            function test(name: string, context: Context, isEnd: boolean) {
+                it(name, () => {
+
+                    const parser = createParserObject(isEnd ? opts.source : `${opts.source} `, undefined);
+
+                    assert.match({
+                        token: scan(parser, context),
+                        value: parser.tokenValue,
+                        line: parser.line,
+                        column: parser.column,
+                    }, {
+                        token: Token.StringLiteral,
+                        value: opts.value,
+                        line: opts.line,
+                        column: opts.column,
+                    });
+                });
+            }
+
+            // Sloppy mode
+            test(`should scan ${name}`, Context.Empty, false);
+
+            // Strict mode
+            test(`should scan ${name}`, Context.Strict, false);
+        }
+
+        pass('scans \'\'', {
+            source: '\'\'',
+            value: '',
+            raw: '\'\'',
+            line: 1,
+            column: 2,
+        });
+
+        pass('scans ""', {
+            source: '""',
+            value: '',
+            raw: '""',
+            line: 1,
+            column: 2,
+        });
+
+        pass('scans \'123\'', {
+            source: '\'123\'',
+            value: '123',
+            raw: '\'123\'',
+            line: 1,
+            column: 5,
+        });
+
+        pass('scans "123"', {
+            source: '"123"',
+            value: '123',
+            raw: '"123"',
+            line: 1,
+            column: 5,
+        });
+
+        const inputData: any = [
+            // Empty string literal
+            [Context.Empty, '""', '', Token.StringLiteral],
+            [Context.Empty, '"0"', '0', Token.StringLiteral],
+            [Context.Empty, '"a"', 'a', Token.StringLiteral],
+            [Context.Empty, '"abc"', 'abc', Token.StringLiteral],
+
+            // Letters small
+            [Context.Empty, '"\\a"', '\a', Token.StringLiteral],
+            [Context.Empty, '"\\b"', '\b', Token.StringLiteral],
+            [Context.Empty, '"\\c"', '\c', Token.StringLiteral],
+
+            // English capitals
+            [Context.Empty, '"K"', 'K', Token.StringLiteral],
+            [Context.Empty, '"M"', 'M', Token.StringLiteral],
+            [Context.Empty, '"N"', 'N', Token.StringLiteral],
+
+            // Russian small
+            [Context.Empty, '"п"', 'п', Token.StringLiteral],
+            [Context.Empty, '"ф"', 'ф', Token.StringLiteral],
+            [Context.Empty, '"ч"', 'ч', Token.StringLiteral],
+            [Context.Empty, '"с"', 'с', Token.StringLiteral],
+            [Context.Empty, '"т"', 'т', Token.StringLiteral],
+            [Context.Empty, '"о"', 'о', Token.StringLiteral],
+
+            // Russian capitals
+            [Context.Empty, '"Ж"', 'Ж', Token.StringLiteral],
+            [Context.Empty, '"И"', 'И', Token.StringLiteral],
+            [Context.Empty, '"П"', 'П', Token.StringLiteral],
+            [Context.Empty, '"Ш"', 'Ш', Token.StringLiteral],
+            [Context.Empty, '"т"', 'т', Token.StringLiteral],
+            [Context.Empty, '"Ю"', 'Ю', Token.StringLiteral],
+            [Context.Empty, '"Ъ"', 'Ъ', Token.StringLiteral],
+            [Context.Empty, '"Э"', 'Э', Token.StringLiteral],
+            [Context.Empty, '"З"', 'З', Token.StringLiteral],
+            [Context.Empty, '"Р"', 'Р', Token.StringLiteral],
+            [Context.Empty, '"Ф"', 'Ф', Token.StringLiteral],
+
+            // Unicode
+            [Context.Empty, '"\\u{00F8}"', 'ø', Token.StringLiteral],
+            [Context.Empty, '"\\u{0000000000F8}"', 'ø', Token.StringLiteral],
+            [Context.Empty, '"\\u{10FFFF}"', '􏿿', Token.StringLiteral],
+            [Context.Empty, '"\\u000a"', '\n', Token.StringLiteral],
+            [Context.Empty, '"\\u{4f06}"', '伆', Token.StringLiteral],
+            [Context.Empty, '"\\u{6c1a}"', '氚', Token.StringLiteral],
+            [Context.Empty, '"\\u{4c2a}"', '䰪', Token.StringLiteral],
+            [Context.Empty, '"\\u03ef"', 'ϯ', Token.StringLiteral],
+            [Context.Empty, '"\\u03f2"', 'ϲ', Token.StringLiteral],
+            [Context.Empty, '"\\u03f2"', 'ϲ', Token.StringLiteral],
+            [Context.Empty, '"\\u03f6"', '϶', Token.StringLiteral],
+            [Context.Empty, '"\\u03a7"', 'Χ', Token.StringLiteral],
+            [Context.Empty, '"\\u03f0"', 'ϰ', Token.StringLiteral],
+            [Context.Empty, '"\\u4f64"', '佤', Token.StringLiteral],
+            [Context.Empty, '"\\u4f66"', '佦', Token.StringLiteral],
+            [Context.Empty, '"\\u4f68"', '佨', Token.StringLiteral],
+            [Context.Empty, '"\\u80fb"', '胻', Token.StringLiteral],
+            [Context.Empty, '"\\u80fc"', '胼', Token.StringLiteral],
+
+            [Context.Empty, '"\\u8036"', '耶', Token.StringLiteral],
+            [Context.Empty, '"\\u8037"', '耷', Token.StringLiteral],
+
+            // Hex
+            [Context.Empty, '"\\xF3"', 'ó', Token.StringLiteral],
+            [Context.Empty, '"\\x80"', '', Token.StringLiteral],
+            [Context.Empty, '"\xA7"', '§', Token.StringLiteral],
+            [Context.Empty, '"\\xFD"', 'ý', Token.StringLiteral],
+            [Context.Empty, '"\\x78"', 'x', Token.StringLiteral],
+            [Context.Empty, '"\\xFC"', 'ü', Token.StringLiteral],
+            [Context.Empty, '"\\xF9"', 'ù', Token.StringLiteral],
+            [Context.Empty, '"\\xF3"', 'ó', Token.StringLiteral],
+            [Context.Empty, '"\\0x410"', '\u0000x410', Token.StringLiteral],
+
+            // Numbers
+            [Context.Empty, '"\\1"', '\u0001', Token.StringLiteral],
+            [Context.Empty, '"\\2"', '\u0002', Token.StringLiteral],
+            [Context.Empty, '"\\3"', '\u0003', Token.StringLiteral],
+            [Context.Empty, '"\\4"', '\u0004', Token.StringLiteral],
+            [Context.Empty, '"\\5"', '\u0005', Token.StringLiteral],
+            // [Context.Empty, '"\\8"', "\u0008", Token.StringLiteral],
+            // [Context.Empty, '"\\9"', "\u0009", Token.StringLiteral],
+            [Context.Empty, '"\\777"', '?7', Token.StringLiteral],
+            [Context.Empty, '"\\123"', 'S', Token.StringLiteral],
+            [Context.Empty, '"\\456"', '%6', Token.StringLiteral],
+            [Context.Empty, '"\\470"', '\'0', Token.StringLiteral],
+            [Context.Empty, '"\\00001"', '\u000001', Token.StringLiteral],
+            [Context.Empty, '"\\0000000"', '\u00000000', Token.StringLiteral],
+            [Context.Empty, '"\\7777447698834478562"', '?77447698834478562', Token.StringLiteral],
+            [Context.Empty, '"\\2111"', '1', Token.StringLiteral],
+            [Context.Empty, '"\\5111"', ')11', Token.StringLiteral],
+            [Context.Empty, '"\\5a"', '\u0005a', Token.StringLiteral],
+            [Context.Empty, '"\\500"', '(0', Token.StringLiteral],
+
+            // \\0 - \\7 (with possible leading zeroes)
+            [Context.Empty, '"\\6"', '\u0006', Token.StringLiteral],
+            [Context.Empty, '"\\7"', '\u0007', Token.StringLiteral],
+
+            // \\10 - \\77 (with possible leading zeroes)'
+            [Context.Empty, '"\\10"', '\b', Token.StringLiteral],
+            [Context.Empty, '"\\77"', '?', Token.StringLiteral],
+            [Context.Empty, '"\\13"', '\u000b', Token.StringLiteral],
+            [Context.Empty, '"\\61"', '1', Token.StringLiteral],
+            [Context.Empty, '"\\62"', '2', Token.StringLiteral],
+            [Context.Empty, '"\\57"', '/', Token.StringLiteral],
+            [Context.Empty, '"\\26"', '\u0016', Token.StringLiteral],
+
+            // \\100 - \\377 (with possible leading zeroes)
+            [Context.Empty, '"\\353"', 'ë', Token.StringLiteral],
+            [Context.Empty, '"\\360"', 'ð', Token.StringLiteral],
+            [Context.Empty, '"\\374"', 'ü', Token.StringLiteral],
+            [Context.Empty, '"\\267"', '·', Token.StringLiteral],
+            [Context.Empty, '"\\136"', '^', Token.StringLiteral],
+        ];
+
+        for (const [ctx, source, parsed, token] of inputData) {
+            it(`scans '${source}'`, () => {
+                const parser = createParserObject(source, undefined);
+                const found = scan(parser, ctx);
+
+                assert.match({
+                    token: tokenDesc(found),
+                    value: parser.tokenValue,
+                    line: parser.line,
+                    column: parser.column,
+                }, {
+                    token: tokenDesc(token),
+                    value: parsed,
+                    line: 1,
+                    column: source.length,
+                });
+            });
+        }
+    });
+});
