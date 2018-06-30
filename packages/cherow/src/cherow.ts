cf4f658ad4a100f20c70479b9f9e0b5841d1d57e
薛定谔的猫 <hh_2013@foxmail.com>
2018-07-01 01:14:12 +0800

    feat(cherow): add tokenize

--- packages/cherow/src/cherow.ts
+++ packages/cherow/src/cherow.ts
@@ -5,3 +5,4 @@ import * as Scanner from './lexer/index';
 export const version = '__VERSION__';
 
 export { ESTree, Scanner, parseSource };
+export * from './tokenize/index';
--- packages/cherow/src/tokenize/index.ts
+++ packages/cherow/src/tokenize/index.ts
@@ -1,20 +1,41 @@
 import { State } from '../types';
+
 import { Token, KeywordDescTable } from '../token';
+
+export function tokenize(state: State, context: Context) {
+  // TODO:
+}
 
 // TODO! Optimize and refactor this!
 
-export function getTokenValue(state: State, t: Token) {
+
+
+function getTokenValue(state: State, t: Token) {
+
   if (t & Token.Punctuator) return KeywordDescTable[t & Token.Type];
+
   return state.source.slice(state.startIndex, state.index);
+
 }
 
-export function convertTokenType(t: Token): string {
+
+
+function convertTokenType(t: Token): string {
+
   if (t & Token.Identifier) return 'Identifier';
+
   if (t & Token.Punctuator) return 'Punctuator';
+
   if (t & Token.NumericLiteral) return 'Numeric';
+
   if ((t & Token.StringLiteral) === Token.StringLiteral) return 'String';
+
   if (t & Token.RegularExpression) return 'Regular Expression';
+
   if (t & Token.Template) return 'Template';
+
   if (t & (Token.Reserved | Token.FutureReserved)) return 'Keyword';
+
   return 'Boolean'; // true / false
+
 }
